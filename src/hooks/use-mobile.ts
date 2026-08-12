41855769ce6f346558fe6e35b8c270550ca2a875
navaneethan-interloid <navaneethan@interloid.com>
2026-08-12 16:30:09 +0530

    update theme

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,19 +1,25 @@
-// import * as React from "react"
+import * as React from 'react'
 
-// const MOBILE_BREAKPOINT = 768
+const MOBILE_BREAKPOINT = 768
 
-// export function useIsMobile() {
-//   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+const subscribe = (callback: () => void) => {
+  const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
 
-//   React.useEffect(() => {
-//     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-//     const onChange = () => {
-//       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-//     }
-//     mql.addEventListener("change", onChange)
-//     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-//     return () => mql.removeEventListener("change", onChange)
-//   }, [])
+  mql.addEventListener('change', callback)
 
-//   return !!isMobile
-// }
+  return () => {
+    mql.removeEventListener('change', callback)
+  }
+}
+
+const getSnapshot = () => {
+  return window.innerWidth < MOBILE_BREAKPOINT
+}
+
+const getServerSnapshot = () => {
+  return false
+}
+
+export function useIsMobile() {
+  return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
+}
